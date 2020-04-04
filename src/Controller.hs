6c0e8f563772c369501ec67fc6b1b7e5187c6758
blund <boergelun@gmail.com>
2020-04-04 17:38:01 +0200

    remove deps, change buttonstates form map

--- src/Controller.hs
+++ src/Controller.hs
@@ -27,26 +27,84 @@ import qualified Data.Map.Strict               as M
 import           Data.Word                      ( Word8 )
 import           Foreign.C.String               ( withCString )
 
-data Intent = Left | Right | Up | Down | Idle | Quit deriving Show
+data Intent = Left | Right | Up | Down | Idle | Quit deriving (Show)
 
 data Button = A | B | X | Y | LS | RS | Start | Select | U | D | L | R deriving (Bounded, Enum, Ord, Eq, Show)
-type ButtonStates = M.Map Button ButtonState
-
-initialButtonStates = M.fromList $ map (, Released) ([minBound..maxBound] :: [Button])
-
-type Accessor = ButtonStates -> ButtonState
 
 data ButtonState = Pressed | Released deriving (Show, Eq)
+
 type Transition = (Button, ButtonState)
 
+data ButtonStates = ButtonStates
+  { a :: ButtonState
+  , b :: ButtonState
+  , x :: ButtonState
+  , y :: ButtonState
+  , ls :: ButtonState
+  , rs :: ButtonState
+  , start :: ButtonState
+  , select :: ButtonState
+  , u :: ButtonState
+  , d :: ButtonState
+  , l :: ButtonState
+  , r :: ButtonState
+  } deriving (Show)
+
+
+setButton :: Button -> ButtonState -> ButtonStates -> ButtonStates
+setButton A      s ss = ss { a = s }
+setButton B      s ss = ss { b = s }
+setButton X      s ss = ss { x = s }
+setButton Y      s ss = ss { y = s }
+setButton LS     s ss = ss { ls = s }
+setButton RS     s ss = ss { rs = s }
+setButton Start  s ss = ss { start = s }
+setButton Select s ss = ss { select = s }
+setButton U      s ss = ss { u = s }
+setButton D      s ss = ss { d = s }
+setButton L      s ss = ss { l = s }
+setButton R      s ss = ss { r = s }
+
+getButton :: Button -> ButtonStates -> ButtonState
+getButton A      ss = a ss
+getButton B      ss = b ss
+getButton X      ss = x ss
+getButton Y      ss = y ss
+getButton LS     ss = ls ss
+getButton RS     ss = rs ss
+getButton Start  ss = start ss
+getButton Select ss = select ss
+getButton U      ss = u ss
+getButton D      ss = d ss
+getButton L      ss = l ss
+getButton R      ss = r ss
+
+
+-- initialButtonStates =
+--  M.fromList $ map (, Released) ([minBound .. maxBound] :: [Button])
+
+initialButtonStates = ButtonStates Released
+                                   Released
+                                   Released
+                                   Released
+                                   Released
+                                   Released
+                                   Released
+                                   Released
+                                   Released
+                                   Released
+                                   Released
+                                   Released
+
 mkIntent :: ButtonStates -> [Intent]
-mkIntent bs = [quit, up, down, left, right]
-  where select key intent = if M.lookup key bs == Just Pressed then intent else Idle
-        up = select U Up
-        down = select D Down
-        left = select L Left
-        right = select R Right
-        quit = select Select Quit
+mkIntent ss = [quit, up, down, left, right]
+ where
+  select key intent = if getButton key ss == Pressed then intent else Idle
+  up    = select U Up
+  down  = select D Down
+  left  = select L Left
+  right = select R Right
+  quit  = select Select Quit
 
 applyEvent :: SDL.Event -> ButtonStates -> ButtonStates
 applyEvent e s = applyTransitions s . payloadToTransitions . extractPayload $ e
@@ -55,7 +113,7 @@ applyTransitions :: ButtonStates -> [Transition] -> ButtonStates
 applyTransitions = foldr apply
  where
   apply :: Transition -> ButtonStates -> ButtonStates
-  apply (acc, s) = M.adjust (const s) acc
+  apply (acc, s) = setButton acc s
 
 extractPayload :: SDL.Event -> SDL.EventPayload
 extractPayload (SDL.Event _t p) = p
@@ -63,10 +121,7 @@ extractPayload (SDL.Event _t p) = p
 payloadToTransitions :: SDL.EventPayload -> [Transition]
 payloadToTransitions (SDL.JoyAxisEvent   k) = fromJoyAxis k
 payloadToTransitions (SDL.JoyButtonEvent k) = [fromJoyButton k]
-payloadToTransitions (SDL.KeyboardEvent k) = fromKeyboardEvent k
-payloadToTransitions _ = []
-
-fromKeyboardEvent (SDL.KeyboardEventData _ _ _ (SDL.Keysym _ SDL.KeycodeEscape _)) = [(Start, Pressed)]
+payloadToTransitions _                      = []
 
 fromJoyButton :: SDL.JoyButtonEventData -> Transition
 fromJoyButton (SDL.JoyButtonEventData _ button state) =
@@ -95,31 +150,3 @@ buttonMap 9 = Start
 buttonStateMap :: SDL.JoyButtonState -> ButtonState
 buttonStateMap SDL.JoyButtonPressed  = Pressed
 buttonStateMap SDL.JoyButtonReleased = Released
-
-{-
-buttonStatesToIntent :: ButtonStates = [Intet]
-
-updateButtonStates :: SDL.Event -> ButtonStates -> ButtonStates
-updateButtonStates = applyTransitions . payloadToTransitions . extractPayload 
-
-applyTransitions :: [Transition] -> ButtonStates -> ButtonStates
-applyTransitions changes states = foldr transitionButton states changes
-
-transitionButton :: Transition -> ButtonStates -> ButtonStates
-transitionButton (button, state) = M.adjust (const state) button
-
-
-
-payloadToTransitions :: SDL.EventPayload -> [Transition]
-payloadToButton (SDL.JoyAxisEvent k) = fromJoyAxis k
-payloadToTransitions (SDL.JoyButtonEvent k) = [fromJoyButton k]
-
-
-
-
-
-
-
-
--}
-
